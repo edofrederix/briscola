aad95cb8b6eee0c37079b67b129e86547422f74e
Edo Frederix <frederix@nrg.eu>
2023-07-06 13:02:43 +0200

    Added point interpolators

--- applications/test/cellSearch/Test-cellSearch.C
+++ applications/test/cellSearch/Test-cellSearch.C
@@ -1,10 +1,74 @@
 #include "arguments.H"
 #include "Time.H"
-#include "uniformMesh.H"
-#include "lineEdge.H"
+#include "fvMesh.H"
+#include "pointInterpolator.H"
 
 using namespace Foam;
 using namespace briscola;
+using namespace fv;
+
+template<class Type>
+void testColocatedInterpolations(const fvMesh& fvMsh)
+{
+    const partLevel& lvl = fvMsh.msh()[0];
+
+    const meshDirection<vector,colocated>& cc =
+        fvMsh.template metrics<colocated>().cellCenters()[0][0];
+
+    vectorList points(lvl.size());
+
+    label c = 0;
+    forAllBlock(lvl, i, j, k)
+    {
+        points[c++] = cc(i,j,k);
+    }
+
+    autoPtr<pointInterpolator> nInterpPtr
+    (
+        pointInterpolator::New(fvMsh,points,"nearest")
+    );
+
+    autoPtr<pointInterpolator> lInterpPtr
+    (
+        pointInterpolator::New(fvMsh,points,"nearest")
+    );
+
+    pointInterpolator& nInterp = nInterpPtr();
+    pointInterpolator& lInterp = lInterpPtr();
+
+    meshField<Type,colocated> field("field", fvMsh);
+    field = Zero;
+
+    forAll(field, l)
+    forAll(field[l], d)
+    forAllCells(field[l][d], i, j, k)
+    {
+        field[l][d](i,j,k) = (i+j+k+l+d)*pTraits<Type>::one;
+    }
+
+    List<Type> nValues(nInterp(field));
+    List<Type> lValues(lInterp(field));
+
+    c = 0;
+    forAllBlock(lvl, i, j, k)
+    {
+        if (nValues[c] != field[0][0](i,j,k))
+            FatalErrorInFunction
+                << "test 3 failed" << endl << abort(FatalError);
+
+        c++;
+    }
+
+    c = 0;
+    forAllBlock(lvl, i, j, k)
+    {
+        if (lValues[c] != field[0][0](i,j,k))
+            FatalErrorInFunction
+                << "test 4 failed" << endl << abort(FatalError);
+
+        c++;
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -23,9 +87,9 @@ int main(int argc, char *argv[])
         )
     );
 
-    autoPtr<mesh> mshPtr(mesh::New(meshDict));
+    fvMesh fvMsh(meshDict, runTime);
 
-    const mesh& msh = mshPtr();
+    const mesh& msh = fvMsh.msh();
 
     // Find cell centers and see if they match
 
@@ -35,9 +99,11 @@ int main(int argc, char *argv[])
 
         forAllBlock(lvl, i, j, k)
         {
-            const vectorBlock points(lvl.points().cellPoints(i,j,k));
+            const vertexVector vertices(lvl.points().cellPoints(i,j,k));
 
-            const vector point(average(points));
+            vector point = Zero;
+            for(int v = 0; v < 8; v++)
+                point += vertices[v]/8.0;
 
             labelVector ijk = msh.findCell(point, l);
 
@@ -52,4 +118,15 @@ int main(int argc, char *argv[])
                     << "Test 2 failed" << endl << abort(FatalError);
         }
     }
+
+    // Interpolate points
+
+    testColocatedInterpolations<scalar>(fvMsh);
+    testColocatedInterpolations<vector>(fvMsh);
+    testColocatedInterpolations<tensor>(fvMsh);
+    testColocatedInterpolations<sphericalTensor>(fvMsh);
+    testColocatedInterpolations<symmTensor>(fvMsh);
+    testColocatedInterpolations<diagTensor>(fvMsh);
+    testColocatedInterpolations<faceScalar>(fvMsh);
+    testColocatedInterpolations<faceVector>(fvMsh);
 }
---
+++ libraries/briscolaFiniteVolume/functionObjects/pointInterpolators/nearestPointInterpolator/nearestPointInterpolator.C
@@ -0,0 +1,89 @@
+#include "nearestPointInterpolator.H"
+#include "geometry.H"
+#include "addToRunTimeSelectionTable.H"
+
+namespace Foam
+{
+
+namespace briscola
+{
+
+namespace fv
+{
+
+defineTypeNameAndDebug(nearestPointInterpolator, 0);
+
+addToRunTimeSelectionTable
+(
+    pointInterpolator,
+    nearestPointInterpolator,
+    dictionary
+);
+
+nearestPointInterpolator::nearestPointInterpolator
+(
+    const fvMesh& fvMsh,
+    const vectorList& points
+)
+:
+    pointInterpolator(fvMsh, points)
+{}
+
+nearestPointInterpolator::nearestPointInterpolator
+(
+    const nearestPointInterpolator& interp
+)
+:
+    pointInterpolator(interp)
+{}
+
+#define INTERPFUNC(TYPE)                                                    \
+List<TYPE> nearestPointInterpolator::operator()                             \
+(                                                                           \
+    const meshField<TYPE,colocated>& field                                  \
+)                                                                           \
+{                                                                           \
+    return this->interp(field);                                             \
+}
+
+INTERPFUNC(scalar);
+INTERPFUNC(vector);
+INTERPFUNC(tensor);
+INTERPFUNC(sphericalTensor);
+INTERPFUNC(symmTensor);
+INTERPFUNC(diagTensor);
+INTERPFUNC(faceScalar);
+INTERPFUNC(faceVector);
+
+#undef INTERPFUNC
+
+template<class Type>
+List<Type> nearestPointInterpolator::interp
+(
+    const meshField<Type,colocated>& field
+)
+{
+    const meshDirection<Type,colocated>& f = field[0][0];
+
+    List<Type> values(points_.size());
+
+    forAll(points_, i)
+    {
+        if (indices_[i] != -unitXYZ)
+        {
+            values[i] = f(indices_[i]);
+        }
+        else
+        {
+            values[i] = Zero;
+        }
+    }
+
+    return values;
+}
+
+}
+
+}
+
+}
