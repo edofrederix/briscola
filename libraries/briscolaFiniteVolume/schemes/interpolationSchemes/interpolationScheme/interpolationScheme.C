f1f885c2ca04d1087e4beecc9efd741105df3391
Edo Frederix <frederix@nrg.eu>
2024-05-07 12:30:25 +0200

    Added colocated/staggered interpolation functions

--- libraries/briscolaFiniteVolume/schemes/interpolationSchemes/interpolationScheme/interpolationScheme.C
+++ libraries/briscolaFiniteVolume/schemes/interpolationSchemes/interpolationScheme/interpolationScheme.C
@@ -167,6 +167,91 @@ tmp<meshField<LowerFaceSpace<Type>,staggered>> stagFaceInterp
     return tInterp;
 }
 
+template<class Type>
+tmp<meshField<Type,colocated>> coloInterp
+(
+    const meshField<Type,staggered>& field
+)
+{
+    tmp<meshField<Type,colocated>> tInterp
+    (
+        new meshField<Type,colocated>
+        (
+            "coloInterp("+field.name()+")",
+            field.fvMsh()
+        )
+    );
+
+    meshField<Type,colocated>& Interp = tInterp.ref();
+
+    Interp = Zero;
+
+    forAllCells(Interp, i, j, k)
+        for (int d = 0; d < 3; d++)
+            Interp(i,j,k) +=
+                0.5/3.0*(field(d,i,j,k) + field(upperNei(i,j,k,d)));
+
+    return tInterp;
+}
+
+template<class Type>
+tmp<meshField<Type,colocated>> coloInterp
+(
+    const tmp<meshField<Type,staggered>>& tField
+)
+{
+    tmp<meshField<Type,colocated>> tInterp
+    (
+        stagInterp(tField())
+    );
+
+    tField.clear();
+
+    return tInterp;
+}
+
+
+template<class Type>
+tmp<meshField<LowerFaceSpace<Type>,colocated>> coloFaceInterp
+(
+    const meshField<Type,staggered>& field
+)
+{
+    tmp<meshField<LowerFaceSpace<Type>,colocated>> tInterp
+    (
+        new meshField<LowerFaceSpace<Type>,colocated>
+        (
+            "coloFaceInterp("+field.name()+")",
+            field.fvMsh()
+        )
+    );
+
+    meshField<LowerFaceSpace<Type>,colocated>& Interp = tInterp.ref();
+
+    Interp = Zero;
+
+    forAllFaces(Interp, fd, i, j, k)
+        Interp(i,j,k)[fd] = field(fd,i,j,k);
+
+    return tInterp;
+}
+
+template<class Type>
+tmp<meshField<LowerFaceSpace<Type>,colocated>> coloFaceInterp
+(
+    const tmp<meshField<Type,staggered>>& tField
+)
+{
+    tmp<meshField<LowerFaceSpace<Type>,colocated>> tInterp
+    (
+        coloFaceInterp(tField())
+    );
+
+    tField.clear();
+
+    return tInterp;
+}
+
 }
 
 }
