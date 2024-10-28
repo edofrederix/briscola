dbc713418dc6726f990e00624d9c4e352a1cef91
Habiyaremye, V <habiyaremye@nrg.eu>
2024-10-28 17:17:34 +0100

    Small fix in PoiseuilleIBM cases
    Sampling at the edge of the domain was causing lineSample to miss some values.
    
    Also added analytical solution to the plot script.

--- cases/briscolaColocated/PoiseuilleIBM/plot.py
+++ cases/briscolaColocated/PoiseuilleIBM/plot.py
@@ -9,9 +9,15 @@ fs.prep(plt)
 
 y = np.loadtxt('postProcessing/y/20/sample.txt')
 
+nu = 0.1
+G = 1
+y_an = np.linspace(-0.5,0.5,100)
+u_an = [G/(2*nu)*(y+0.5)*(1-(y+0.5)) for y in y_an]
+
 fig = plt.figure()
 
 plt.plot(y[:,1], y[:,4], label='$u_x(y)$')
+plt.plot(y_an, u_an, ':k', label='analytical')
 
 plt.xlabel('$x$, $y$')
 plt.ylabel('$u_x$, $u_y$')
